1909e205096bae39b7d946ab8c8975b68be3cbc0
thibgil <zaideby@gmail.com>
2025-10-23 18:05:13 +0200

    implemented a route with a POST request included in a view for making some votes

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,8 @@
 from . import views
 from django.urls import path
 
+app_name = "polls"
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("<int:question_id>/", views.detail, name="detail"),
